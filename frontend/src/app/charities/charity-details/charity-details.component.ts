ccbfd54641a2011d126a94f2de18d5f3e7065c31
Brian Nguyen <bluefish7391@gmail.com>
2025-03-07 18:34:54 -0500

    update charity details component

--- frontend/src/app/charities/charity-details/charity-details.component.ts
+++ frontend/src/app/charities/charity-details/charity-details.component.ts
@@ -9,8 +9,29 @@ import { ActivatedRoute, Router } from '@angular/router';
     selector: 'charities-charity-details',
     templateUrl: './charity-details.component.html',
     styleUrl: './charity-details.component.scss',
-    imports: []
+    imports: [CommonModule, FormsModule]
 })
 export class CharityDetailsComponent {
+    charity = new Charity();
+      constructor(
+        private xapiService: XapiService,
+        private activatedRoute: ActivatedRoute,
+        private router: Router
+      ) {
+      }
     
+      async ngOnInit() {
+        const id = this.activatedRoute.snapshot.params['id'];
+        if(id && id!='new') {
+          this.charity = await this.xapiService.getCharity(id);
+          if(!this.charity) {
+            this.charity = new Charity();
+          }
+        }
+      }
+    
+      async saveClicked() {
+        await this.xapiService.saveCharity(this.charity!);
+        this.router.navigateByUrl('/charities/list');
+      }
 }
