0625db591caf2a99bba45a6715d64d870139db9c
Brian Nguyen <bluefish7391@gmail.com>
2025-04-06 22:13:05 -0400

    add role-related classes to backend

--- backend/src/shared/kinds.ts
+++ backend/src/shared/kinds.ts
@@ -37,3 +37,32 @@ export class CharityLocation {
     streetNumber: number | undefined;
     charityID: string | undefined;
 }
+
+/*
+export class User {
+    id: string | undefined;
+    name: string | undefined;
+    placeOfWorkID: string | undefined;
+    roles: {
+        roleID: string,
+        enabled: boolean
+    }[] = [];
+}
+*/
+
+export class Role {
+    id: string | undefined;
+    siteID: string | undefined;
+    name: string | undefined;
+    description: string | undefined;
+    permissions: {
+        permissionID: string,
+        enabled: boolean
+    }[] = [];
+}
+
+export class Permission {
+    id: string | undefined;
+    name: string | undefined;
+    description: string | undefined;
+}
