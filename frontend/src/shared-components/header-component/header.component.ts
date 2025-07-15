3f0246d5d056066b0685f840c6b2dfa48b21ac6e
Brian Nguyen <bluefish7391@gmail.com>
2025-07-14 20:14:35 -0400

    display sign in button vs sign out depending on logged in status

--- frontend/src/shared-components/header-component/header.component.ts
+++ frontend/src/shared-components/header-component/header.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
+import { SignOutButtonComponent } from '../sign-out-component/sign-out.component';
 
 @Component({
-  selector: 'page-header',
-  imports: [CommonModule, RouterModule],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+    selector: 'page-header',
+    imports: [CommonModule, RouterModule, SignOutButtonComponent],
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  constructor(
-    private router: Router
-  ) {
-  }
+    loggedIn = false;
+    constructor(
+        private router: Router
+    ) {
+    }
 
-  async ngOnInit() {
-  }
+    async ngOnInit() {
+        // Add ability to check whether user is logged in to change loggedIn
+        // Greet user by name if logged in
+    }
 }
