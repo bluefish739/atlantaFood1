4c585a58a0f9b62ada41d55202b6c5500b6efe3e
Brian Nguyen <bluefish7391@gmail.com>
2025-07-16 15:52:17 -0400

    revamp home page template

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../../shared-components/header-component/header.component';
-import { FooterComponent } from '../../shared-components/footer-component/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
