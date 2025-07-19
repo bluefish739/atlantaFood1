1883fd773e76b7c0fa961fc133cc81893bf23852
Brian Nguyen <bluefish7391@gmail.com>
2025-07-18 23:39:56 -0400

    replace sign out component with button in nav bar

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -3,21 +3,31 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 import { StoreDashboardComponent } from './store-dashboard.component';
 import { PantryDashboardComponent } from './pantry-dashboard.component.';
 import { VolunteerDashboardComponent } from './volunteer-dashboard.component';
 import { AdminDashboardComponent } from './admin-dashboard.component';
+import { sessionAuthenticator } from '../utilities/session-authentication';
 
 @Component({
   selector: 'dashboard',
-  imports: [RouterModule, FormsModule, CommonModule, StoreDashboardComponent, 
+  imports: [RouterModule, FormsModule, CommonModule, StoreDashboardComponent,
     PantryDashboardComponent, VolunteerDashboardComponent, AdminDashboardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
   userType = "";
-  constructor(public authService: AuthService) {
+  constructor(
+    public authService: AuthService,
+    private router: Router
+  ) {
     this.userType = "store";
+  }
+
+  signOut() {
+    sessionAuthenticator.deleteCookie("sessionID");
+    this.router.navigateByUrl("users/login");
   }
 }
--- frontend/src/app/users/users-list/users-list.component.ts
+++ frontend/src/app/users/users-list/users-list.component.ts
@@ -3,13 +3,12 @@ import { XapiService } from '../../xapi.service';
 import { User } from '../../kinds';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
-import { SignOutButtonComponent } from '../../../shared-components/sign-out-component/sign-out.component';
 import { HeaderComponent } from '../../../shared-components/header-component/header.component';
 import { FooterComponent } from '../../../shared-components/footer-component/footer.component';
 
 @Component({
   selector: 'users-user-list',
-  imports: [CommonModule, RouterModule, SignOutButtonComponent, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss'
 })
