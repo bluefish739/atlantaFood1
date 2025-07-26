c8e22696d0528181a8274c64624325ffb347a60f
Brian Nguyen <bluefish7391@gmail.com>
2025-07-26 11:23:37 -0400

    create build.bat file

---
+++ build.bat
@@ -0,0 +1,24 @@
+cd /d %~dp0
+
+cd backend
+call npm run build
+if %ERRORLEVEL% NEQ 0 (
+    echo Backend compilation failed.
+    goto end
+) else (
+    echo Backend successfully compiled.
+)
+cd ..
+cd frontend
+call npm run build
+if %ERRORLEVEL% NEQ 0 (
+    echo Frontend compilation failed.
+    goto end
+) else (
+    echo Frontend successfully compiled.
+)
+cd ..
+call firebase serve
+
+:end
+cd /d %~dp0
