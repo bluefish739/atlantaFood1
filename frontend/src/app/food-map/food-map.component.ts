e32d0644f0bf6c12782c714bba2acb51c980cf2a
Brian Nguyen <bluefish7391@gmail.com>
2025-10-04 15:12:06 -0400

    feat (food-map.component): add basic template for page

--- frontend/src/app/food-map/food-map.component.ts
+++ frontend/src/app/food-map/food-map.component.ts
@@ -2,22 +2,28 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { XapiService } from '../xapi.service';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { HomeHeaderComponent } from '../../shared-components/home-header/home-header.component';
 
 @Component({
-  selector: 'food-map',
-  imports: [CommonModule, RouterModule, HomeHeaderComponent],
-  templateUrl: './food-map.component.html',
-  styleUrl: './food-map.component.scss'
+    selector: 'food-map',
+    imports: [CommonModule, RouterModule, HomeHeaderComponent, MatExpansionModule],
+    templateUrl: './food-map.component.html',
+    styleUrl: './food-map.component.scss'
 })
 export class FoodMapComponent {
-  constructor(
-    private xapiService: XapiService,
-    private router: Router
-  ) {
-  }
+    sites: any[] = [];
+    constructor(
+        private xapiService: XapiService,
+        private router: Router
+    ) {
+    }
 
-  async ngOnInit() {
-    
-  }
+    async ngOnInit() {
+        
+    }
+
+    onPanelOpened(index: number): void {
+        console.log('Opened pantry at index:' + index);
+    }
 }
