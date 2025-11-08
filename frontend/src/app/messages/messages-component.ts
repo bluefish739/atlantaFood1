a2dee647826da14b1af1c5f24a9613386f39686f
Brian Nguyen <bluefish7391@gmail.com>
2025-11-08 18:07:46 -0500

    feat (messages): add reactive input form to search for organizations with autocomplete

--- frontend/src/app/messages/messages-component.ts
+++ frontend/src/app/messages/messages-component.ts
@@ -3,18 +3,24 @@ import { XapiService } from '../xapi.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Message, Organization } from '../../../../shared/src/kinds';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from '../../shared-components/header-component/header.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
     selector: 'messages',
     templateUrl: './messages-component.html',
     styleUrl: './messages-component.scss',
-    imports: [CommonModule, RouterModule, FormsModule, HeaderComponent]
+    imports: [CommonModule, RouterModule, FormsModule, HeaderComponent, ReactiveFormsModule, MatFormFieldModule, MatAutocompleteModule, MatInputModule]
 })
 export class MessagesComponent {
     organizations: Organization[] = [];
     selectedOrganization = new Organization();
+    organizationSearchForm = new FormGroup({
+        searchedOrganization: new FormControl('')
+    });
     newMessageText = "";
     messages: Message[] = [];
     constructor(private xapiService: XapiService) {
@@ -43,5 +49,9 @@ export class MessagesComponent {
         await this.xapiService.sendMessageToOrganization(message);
         this.newMessageText = "";
         this.messages = await this.xapiService.getMessagesWithOrganization(this.selectedOrganization.id!);
+    }
+
+    addChat() {
+        
     }
 }
